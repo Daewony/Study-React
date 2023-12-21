7235d80f19c50484a7836f6a5b5aad0f1151d673
Daewony <alexnori.com@gmail.com>
2023-12-21 20:38:04 +0900

    first commit

---
+++ to-do-list/src/components/EditTodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const EditTodoForm = ({ editTodo,task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  };
+
+  return (
+    <form className="EditTodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="작업 수정"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        작업 수정
+      </button>
+    </form>
+  );
+};
---
+++ to-do-list/src/components/TodoForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+
+export const TodoForm = ({addTodo}) => {
+  const [value, setValue] = useState("")
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    addTodo(value);
+
+    setValue("")
+  }
+
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="작업 추가"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        추가
+      </button>
+    </form>
+  );
+}
